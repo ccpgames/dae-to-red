9ad07538e1d6490bf62954892f09ec82c2725ba5
Regner Blok-Andersen <shadowdf@gmail.com>
2015-01-09 17:13:28 +0000

    Adding a basic setup.py file, still needs a fair bit of work.

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+
+from pip.req    import parse_requirements
+from setuptools import setup, find_packages
+
+from dae_to_red import __version__
+
+install_reqs = parse_requirements('requirements.txt')
+reqs         = [str(ir.req) for ir in install_reqs]
+
+setup(
+    name='dae-to-red',
+    version=__version__,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=reqs,
+    entry_points='''
+    ''',
+)
